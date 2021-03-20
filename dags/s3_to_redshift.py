bbc8e04e26e77f29813233857c6d9de72981b265
EBolle <ernstbolle@gmail.com>
2021-03-20 14:14:27 +0100

    Commented out the __init__.py files, keep it simple for now

--- dags/s3_to_redshift.py
+++ dags/s3_to_redshift.py
@@ -8,7 +8,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-from sql import create_tables import staging_events, staging_songs
+from sql import create_tables
 
 
   # DAG setup
