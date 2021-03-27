9256d86a925835d56b63fdc77bc22fc6dbb0e9f7
EBolle <ernstbolle@gmail.com>
2021-03-27 06:55:10 +0100

    Added the framework for the data quality checks

---
+++ dags/s3_to_redshift_data_quality_checks.py
@@ -0,0 +1,60 @@
+# This DAG checks the data quality of the fact and dimension tables
+
+import datetime
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.sql import SQLCheckOperator
+
+from sql import quality_checks
+
+# DAG setup
+
+default_args = {'owner': 'airflow',
+                'depends_on_past': False,
+                'retries': 0,
+                'retry_delay': timedelta(minutes=5),
+                'email_on_retry': False}
+
+dag = DAG('s3_to_redshift_data_quality_checks',
+          description='Checks the data quality of the fact and dimension tables in Redshift',
+          start_date=datetime.datetime.now(),
+          schedule_interval='@once',
+          default_args=default_args,
+          catchup=False)
+
+# Operators
+
+start_operator = DummyOperator(
+    task_id='begin_data_quality_checks',
+    dag=dag)
+
+dq_songplays = SQLCheckOperator(task_id='dq_songplays',
+                                dag=dag,
+                                sql=quality_checks.songplays)
+
+dq_users = SQLCheckOperator(task_id='dq_users',
+                            dag=dag,
+                            sql=quality_checks.users)
+
+dq_songs = SQLCheckOperator(task_id='dq_songs',
+                            dag=dag,
+                            sql=quality_checks.songs)
+
+dq_artists = SQLCheckOperator(task_id='dq_artists',
+                              dag=dag,
+                              sql=quality_checks.artists)
+
+dq_time = SQLCheckOperator(task_id='dq_time',
+                           dag=dag,
+                           sql=quality_checks.time)
+
+end_operator = DummyOperator(
+    task_id='end_data_quality_checks',
+    dag=dag)
+
+# Order of execution
+
+start_operator >> [dq_songplays, dq_users, dq_songs, dq_artists, dq_time]
+[dq_songplays, dq_users, dq_songs, dq_artists, dq_time] >> end_operator
--- dags/sql/quality_checks.py
+++ dags/sql/quality_checks.py
@@ -1,1 +1,23 @@
-# SQL statements that can be used for quality checks given the SQLCheckOperator
+# SQL statements that can be used for quality checks given the SQLCheckOperator so every statement that returns a 0
+# or False will raise an error (https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/operators/sql/index.html)
+
+
+songplays = """
+
+"""
+
+users = """
+
+"""
+
+songs = """
+
+"""
+
+artists = """
+
+"""
+
+time = """
+
+"""
